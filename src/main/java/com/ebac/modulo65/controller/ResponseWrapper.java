e8f7e994d58eb5ac24c673cc15e553eb7fee8f8c
Bladimir Landa <bladimircarrillolanda02@gmail.com>
2025-03-12 17:59:55 -0600

    Update

--- src/main/java/com/ebac/modulo65/controller/ResponseWrapper.java
+++ src/main/java/com/ebac/modulo65/controller/ResponseWrapper.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 
 //@Getter-@RequiredArgsConstructor: Lombok Annotation
-@Getter @RequiredArgsConstructor
+@Getter
+@RequiredArgsConstructor
 public class ResponseWrapper<T> {
 
     private final boolean success;
